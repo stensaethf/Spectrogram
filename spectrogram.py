4224e1d955de695736c8e959f132db3c3ee5a221
Frederik Stensaeth <stensaethf@carleton.edu>
2015-10-31 14:42:08 -0500

    open file and get window

--- spectrogram.py
+++ spectrogram.py
@@ -7,6 +7,7 @@ the spectrogram of that file.
 '''
 
 import sys
+import os.path
 
 def printError():
 	"""
@@ -19,9 +20,42 @@ def printError():
 	print('Usage: $ python3 spectrogram.py <wav file to be visualized>')
 	sys.exit()
 
-def spectrogram():
+def spectrogram(f):
 	"""
 	"""
+	# Step 1: open wav file and get bytes.
+	# Makes sure that we have been given a proper file to work with.
+	try:
+		f = wave.open(f, 'r')
+		params = f.getparams()
+		channels = params[0]
+		width = params[1]
+		rate = params[2]
+		frames = params[3]
+
+		byte_list = [] #f.readframes(frames)
+		for i in range(frames):
+			byte_list.append(f.readframes(1))
+
+		f.close()
+	except:
+		printError()
+
+	# Step 2: get windows
+	frames_per_window = rate / 4 # 25ms
+	step_size = rate / 10 # 10ms
+	for start in range(0, frames, step_size)
+		if start + frames_per_window < len(byte_list):
+			window = byte_list[start:start + frames_per_window]
+
+			Xx
+
+	# Step 3: fourier transform --> numpy
+
+	# Step 4: frequency magnitudes
+
+	# Step 5: visualize
+
 	return None
 
 def visualizeFrequency(xx):
